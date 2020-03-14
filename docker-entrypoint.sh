865497bcd5f0cfa914bd2e279b81383494d73c83
Richard Kendall <richard.kendall@gmail.com>
2020-03-14 19:56:13 +1100

    adding extra config to support caching

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${REGION} ${TABLE} ${REALM}' < /aws.pam > /etc/pam.d/aws
+envsubst '${REGION} ${TABLE} ${REALM} ${CACHE_FOLDER} ${CACHE_DURATION}' < /aws.pam > /etc/pam.d/aws
 
 exec "$@"
